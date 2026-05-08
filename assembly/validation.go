8f4b60032745ded8a574e0ba8c649e3a9dedd8ba
Chisanan232 <chi10211201@cycu.org.tw>
2026-05-08 11:16:03 +0800

    ✨ (validation): Surface option errors from errs field before required-field checks

--- assembly/validation.go
+++ assembly/validation.go
@@ -3,6 +3,10 @@ package assembly
 import "strings"
 
 func validateRuntimeOptions(opts runtimeOptions) error {
+	if len(opts.errs) > 0 {
+		return opts.errs[0]
+	}
+
 	if strings.TrimSpace(opts.gatewayURL) == "" {
 		return ErrInvalidGateway
 	}
